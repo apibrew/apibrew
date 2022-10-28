dcf3cbde3342077bae59e5be00da8c61c0b1bb65
Taleh Ibrahimli <talehsmail@gmail.com>
2022-10-29 01:04:45 +0400

    initial setup for data-handler-ui

--- envoy/build-and-run.sh
+++ envoy/build-and-run.sh
@@ -5,5 +5,5 @@ docker build -t dh-envoy:1.0 .
 
 
 echo --- Running my-envoy docker image ---
-docker run -p 9901:9901 -p 8100:8100 -d dh-envoy:1.0
+docker run -p 9901:9901 -p 8100:8100 --name dh-envoy -d dh-envoy:1.0
 
