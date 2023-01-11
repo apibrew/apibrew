0816c0df34dfbcf6020fb750a8796f2fdc65ec75
Taleh Ibrahimli <talehsmail@gmail.com>
2023-01-11 11:05:23 +0100

    +fixes

---
+++ test/bench1/proto_bench_test.go
@@ -0,0 +1,95 @@
+package bench1
+
+import (
+	"data-handler/model"
+	"data-handler/test/data"
+	"google.golang.org/protobuf/proto"
+	"log"
+	"testing"
+	"time"
+)
+
+func BenchmarkSerializeProtobufSmall(b *testing.B) {
+	newInitData := &model.InitData{}
+
+	for i := 0; i < 1000; i++ {
+		newInitData.InitResources = append(newInitData.InitResources, data.PreparePersonResource())
+	}
+
+	data, err := proto.Marshal(newInitData)
+
+	log.Print(len(data), err)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			proto.Marshal(newInitData)
+		}
+	})
+}
+
+func BenchmarkSerializeProtobufMedium(b *testing.B) {
+	newInitData := &model.InitData{}
+
+	for i := 0; i < 100000; i++ {
+		newInitData.InitResources = append(newInitData.InitResources, data.PreparePersonResource())
+	}
+
+	data, err := proto.Marshal(newInitData)
+
+	log.Print(len(data), err)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			proto.Marshal(newInitData)
+		}
+	})
+}
+
+func BenchmarkSerializeProtobufLarge(b *testing.B) {
+	newInitData := &model.InitData{}
+
+	res := data.PreparePersonResource()
+
+	for i := 0; i < 100000000; i++ {
+		newInitData.InitResources = append(newInitData.InitResources, res)
+	}
+
+	data, err := proto.Marshal(newInitData)
+
+	log.Print(len(data), err)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := 0; i < 10; i++ {
+				log.Print(i, time.Now())
+				proto.Marshal(newInitData)
+			}
+		}
+	})
+}
+
+func BenchmarkSerializeProtobufLarge2(b *testing.B) {
+	chunkCount := 100000
+	newInitData := &model.InitData{}
+
+	res := data.PreparePersonResource()
+
+	for i := 0; i < 1000000/chunkCount; i++ {
+		newInitData.InitResources = append(newInitData.InitResources, res)
+	}
+
+	data, err := proto.Marshal(newInitData)
+
+	log.Print(len(data), err)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for j := 0; j < chunkCount; j++ {
+				proto.Marshal(newInitData)
+			}
+		}
+	})
+}
+
+// 651428875
+// 453834792
