a7ef0bb96140e0448fa11db3d284e619be7ed891
Taleh Ibrahimli <talehsmail@gmail.com>
2023-01-29 00:35:27 +0100

    rename cli to dhctl

--- run-tests.sh
+++ run-tests.sh
@@ -8,4 +8,4 @@ su postgres -c 'psql -h 127.0.0.1 -f test/dh_test.sql dh_test'
 cd test || exit
 CGO_ENABLED=0 go test
 
-pg_ctl stop -D /var/lib/postgresql/data
+su postgres -c 'pg_ctl stop -D /var/lib/postgresql/data'
