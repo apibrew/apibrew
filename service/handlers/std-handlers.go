2687af0cdc3c419054cfd2d19754ae67300f0b65
Taleh Ibrahimli <talehsmail@gmail.com>
2023-01-29 02:49:10 +0100

    minor fixes

--- service/handlers/std-handlers.go
+++ service/handlers/std-handlers.go
@@ -4,6 +4,7 @@ import (
 	"github.com/tislib/data-handler/model"
 	"github.com/tislib/data-handler/service"
 	"github.com/tislib/data-handler/service/handler"
+	"github.com/tislib/data-handler/service/system"
 )
 
 type StdHandler interface {
@@ -13,15 +14,18 @@ type StdHandler interface {
 type stdHandler struct {
 	genericHandler    *handler.GenericHandler
 	dataSourceHandler *dataSourceHandler
+	userHandler       *userHandler
 }
 
 func (s *stdHandler) Init(data *model.InitData) {
-	s.genericHandler.Register(s.dataSourceHandler.prepareHandler())
+	s.genericHandler.RegisterWithSelector(s.dataSourceHandler.prepareHandler(), handler.ResourceSelector(system.DataSourceResource))
+	s.genericHandler.RegisterWithSelector(s.userHandler.prepareHandler(), handler.ResourceSelector(system.UserResource))
 }
 
-func NewStdHandler(genericHandler *handler.GenericHandler, dataSourceService service.DataSourceService) StdHandler {
+func NewStdHandler(genericHandler *handler.GenericHandler, dataSourceService service.DataSourceService, userService service.UserService, recordService service.RecordService) StdHandler {
 	return &stdHandler{
 		genericHandler:    genericHandler,
 		dataSourceHandler: &dataSourceHandler{dataSourceService: dataSourceService},
+		userHandler:       &userHandler{userService: userService, recordService: recordService},
 	}
 }
