5c98d26fe499e7b0e90ad2dff136de6f606c8624
Taleh Ibrahimli <talehsmail@gmail.com>
2023-02-21 00:02:09 +0100

    implementation of client package

---
+++ pkg/client/abs.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"context"
+	"github.com/tislib/data-handler/pkg/model"
+	"github.com/tislib/data-handler/pkg/stub"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+type DhClient interface {
+	GetAuthenticationServiceClient() stub.AuthenticationServiceClient
+	GetDataSourceServiceClient() stub.DataSourceServiceClient
+	GetResourceServiceClient() stub.ResourceServiceClient
+	GetRecordServiceClient() stub.RecordServiceClient
+	GetGenericServiceClient() stub.GenericServiceClient
+	GetExtensionServiceClient() stub.ExtensionServiceClient
+	GetUserServiceClient() stub.UserServiceClient
+	GetToken() string
+}
+
+type Entity interface {
+	ToRecord() *model.Record
+	FromRecord(record *model.Record)
+	FromProperties(properties map[string]*structpb.Value)
+	ToProperties(includeTopProperties bool) map[string]*structpb.Value
+	GetResourceName() string
+	GetNamespace() string
+	GetId() string
+}
+
+type Repository[T Entity] interface {
+	Create(ctx context.Context, entity T) (T, error)
+	Update(ctx context.Context, entity T) (T, error)
+	Save(ctx context.Context, entity T) (T, error)
+	Get(ctx context.Context, id string) (T, error)
+	List(ctx context.Context) ([]T, error)
+}
---
+++ pkg/client/extension.go
@@ -0,0 +1,11 @@
+package client
+
+type ExtensionBuilder interface {
+}
+
+type extensionBuilder struct {
+}
+
+func NewExtensionBuilder() ExtensionBuilder {
+
+}
