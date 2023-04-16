28030dd3dfee8c39d94fca1876a5247e70e01395
Taleh Ibrahimli <talehsmail@gmail.com>
2023-04-16 13:09:03 +0400

    +fixes

--- generate.go
+++ generate.go
@@ -3,4 +3,5 @@ package data_handler
 //cp Readme.md docs/content/index.md
 //go:generate sh -c "cd proto; go generate"
 //go:generate sh -c "cd pkg/server/rest; go generate"
+//go:generate cp Readme.md docs/index.md
 //widdershins pkg/server/rest/openapi.yaml -o docs/content/openapi.md
