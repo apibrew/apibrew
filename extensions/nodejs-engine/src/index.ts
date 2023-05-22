ae5860e869ea0a06cae7fc2a70e758ff4fbef3f7
Taleh Ibrahimli <talehsmail@gmail.com>
2023-05-22 20:27:42 +0400

    minor fixes

--- extensions/nodejs-engine/src/index.ts
+++ extensions/nodejs-engine/src/index.ts
@@ -1,5 +1,12 @@
 import {Resource} from "./proto/model/resource_pb";
 import {ResourceClient} from "./proto/stub/resource_grpc_pb";
+import {credentials, Server, ServerCredentials} from "@grpc/grpc-js";
+import {ListResourceRequest} from "./proto/stub/resource_pb";
+import {FunctionService, IFunctionServer} from "./proto/ext/function_grpc_pb";
+import {FunctionCallResponse} from "./proto/ext/function_pb";
+import {ExtensionClient} from "./proto/stub/extension_grpc_pb";
+import {registerExtension} from "./registrator";
+import {HOST, PORT} from "./config";
 
 const resource = new Resource()
 
@@ -7,6 +14,27 @@ resource.setName('Taleh123')
 
 console.log(resource.getName())
 
-const client = new ResourceClient('localhost:9009', null, null)
+registerExtension()
 
-// client.
+const server = new Server();
+
+const functionCallHandler: IFunctionServer['functionCall'] = (call, callback) => {
+    console.log(call.request.getName())
+    console.log(call.request.getEvent())
+
+    const response = new FunctionCallResponse()
+
+    response.setEvent(call.request.getEvent())
+
+    console.log(response.getEvent())
+
+    callback(null, response)
+}
+
+server.addService(FunctionService, {
+    functionCall: functionCallHandler
+})
+
+server.bindAsync(`${HOST}:${PORT}`, ServerCredentials.createInsecure(), () => {
+    server.start();
+});
---
+++ extensions/nodejs-engine/src/proto/gnostic/openapi/v3/annotations_pb.d.ts
@@ -0,0 +1,14 @@
+// package: model
+// file: model/annotations.proto
+
+/* tslint:disable */
+/* eslint-disable */
+
+import * as jspb from "google-protobuf";
+import * as google_protobuf_descriptor_pb from "google-protobuf/google/protobuf/descriptor_pb";
+
+export const hclBlock: jspb.ExtensionFieldInfo<string>;
+
+export const hclLabel: jspb.ExtensionFieldInfo<string>;
+
+export const hclIgnore: jspb.ExtensionFieldInfo<boolean>;
