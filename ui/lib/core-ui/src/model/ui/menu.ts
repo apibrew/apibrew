f44f3d1f5c5956d6bc6b94a8437ae64d20377875
Taleh Ibrahimli <talehsmail@gmail.com>
2023-06-03 21:53:31 +0400

    menu

---
+++ ui/lib/core-ui/src/model/ui/menu.ts
@@ -0,0 +1,36 @@
+
+
+
+// Sub Types
+
+export interface MenuItem {
+     title: string;
+     link?: string;
+     icon?: string;
+     children?: MenuItem[];
+
+}
+
+// Resource Type
+export interface Menu {
+    id: string;
+name: string;
+title: string;
+children: MenuItem[];
+version: number;
+
+}
+// Resource and Property Names
+export const MenuName = "Menu";
+
+export const MenuIdName = "Id";
+
+export const MenuNameName = "Name";
+
+export const MenuTitleName = "Title";
+
+export const MenuChildrenName = "Children";
+
+export const MenuVersionName = "Version";
+
+
