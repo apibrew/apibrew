f2a78c449d1f2cf35163b4bb375e2479945cf984
Taleh Ibrahimli <talehsmail@gmail.com>
2023-06-14 14:32:28 +0400

    +fixes

--- ui/documentation/src/main.tsx
+++ ui/documentation/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Sdk} from "./components/sdk/Sdk.tsx";
+import {Box} from "@mui/material";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Box display='flex' sx={{
+            height: '100vh',
+            width: '100%',
+            background: 'rgb(240,240,240)',
+        }}>
+            <Router>
+                <Routes>
+                    <Route path='/sdk' element={<Sdk/>}/>
+                </Routes>
+            </Router>
+        </Box>
+    </React.StrictMode>,
 )
