a2459c613f606a148140f916cd944022539f521f
Taleh Ibrahimli <talehsmail@gmail.com>
2023-06-26 21:02:02 +0400

    +fixes

--- extensions/nodejs-engine/src/config.ts
+++ extensions/nodejs-engine/src/config.ts
@@ -2,7 +2,7 @@ console.log(process.env)
 export const APBR_ADDR = process.env.APBR_HOST || 'localhost:9009'
 export const ENGINE_ADDR = process.env.ENGINE_HOST || 'localhost:23619'
 export const ENGINE_REMOTE_ADDR = process.env.ENGINE_REMOTE_ADDR || `${ENGINE_ADDR}`
-export const TOKEN = process.env.APBR_TOKE || ''
+export const TOKEN = process.env.APBR_TOKEN || ''
 export const EXTENSION_NAME = 'nodejs-engine'
 
 console.log(`APBR_ADDR: ${APBR_ADDR}; ENGINE_ADDR: ${ENGINE_ADDR}; ENGINE_REMOTE_ADDR: ${ENGINE_REMOTE_ADDR}; TOKEN: ${TOKEN}; EXTENSION_NAME: ${EXTENSION_NAME}`)
