4e699cc0d92931f5f84580adeb9fce91fc051e8c
Taleh Ibrahimli <talehsmail@gmail.com>
2023-07-09 12:56:06 +0300

    major fixes

--- pkg/generator/main.go
+++ pkg/generator/main.go
@@ -1,32 +1,33 @@
 //go:generate go get -u github.com/valyala/quicktemplate/qtc
 //go:generate qtc -dir=templates
 
-package main
+package generator
 
 import (
+	"fmt"
 	"github.com/apibrew/apibrew/pkg/generator/golang"
+	"github.com/apibrew/apibrew/pkg/generator/typescript"
 	"github.com/apibrew/apibrew/pkg/model"
-	"github.com/apibrew/apibrew/pkg/resources"
 )
 
-func main() {
-	//names := []string{"Kate", "Go", "John", "Brad"}
-	//
-	//// qtc creates Write* function for each template function.
-	//// Such functions accept io.Writer as first parameter:
-	//var buf bytes.Buffer
-	//templates.WriteGreetings(&buf, names)
-	//
-	//fmt.Printf("buf=\n%s", buf.Bytes())
+func GenerateResourceCodes(platform string, pkg string, resources []*model.Resource, path string, namespace string) error {
+	switch platform {
+	case "golang":
+		return golang.GenerateGoResourceCode(golang.GenerateResourceCodeParams{
+			Namespace: namespace,
+			Package:   pkg,
+			Resources: resources,
+			Path:      path,
+		})
 
-	//fmt.Printf("%s\n", templates.Hello("Foo"))
-
-	golang.GenerateGoResourceCode(golang.GenerateResourceCodeParams{
-		Package: "modelnew",
-		Resources: []*model.Resource{
-			resources.DataSourceResource,
-		},
-		Path:      "/Users/taleh/Projects/apibrew/pkg/modelnew",
-		Namespace: "system",
-	})
+	case "typescript":
+		return typescript.GenerateResourceCode(typescript.GenerateResourceCodeParams{
+			Namespace: namespace,
+			Package:   pkg,
+			Resources: resources,
+			Path:      path,
+		})
+	default:
+		return fmt.Errorf("Unknown platform: " + platform)
+	}
 }
