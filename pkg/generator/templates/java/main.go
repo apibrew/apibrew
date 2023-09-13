9b39ea2f00984673b8727cacaa6c1d3976caa4aa
Taleh Ibrahimli <talehsmail@gmail.com>
2023-09-13 11:57:27 +0400

    minor fixes

--- pkg/generator/templates/java/main.go
+++ pkg/generator/templates/java/main.go
@@ -8,55 +8,13 @@ import (
 
 func GenerateResourceCode(pkg string, resources []*model.Resource, path string, namespace string) error {
 	for _, resource := range resources {
-
-		fixPropertyNames(resource.Properties)
-
 		if err := generateResourceCode(pkg, resource, path); err != nil {
 			return err
 		}
-
-		//if err := generaDefCode(pkg, resources, path, resource); err != nil {
-		//	return err
-		//}
-
-		//if err := generaMappingCode(pkg, resources, path, resource); err != nil {
-		//	return err
-		//}
 	}
 
 	return nil
 }
-
-func fixPropertyNames(properties []*model.ResourceProperty) {
-	for _, property := range properties {
-		if property.Name == "" {
-		}
-	}
-}
-
-//func generaMappingCode(pkg string, resources []*model.Resource, path string, resource *model.Resource) error {
-//	rawCode := GenerateResourceMappingCode(pkg, resource, resources)
-//
-//	code, err := format.Source([]byte(rawCode))
-//	if err != nil {
-//		code = []byte(rawCode)
-//
-//		log.Warn(err)
-//	}
-//
-//	resourceFileName := slug.Make(resource.Name) + "-mapping.go"
-//
-//	if err := os.MkdirAll(path, 0777); err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	err = os.WriteFile(path+"/"+resourceFileName, code, 0777)
-//
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//}
 
 func generateResourceCode(pkg string, resource *model.Resource, path string) error {
 	code := GenerateClassCode(pkg, resource)
