17f44366f9e96a69e25870067f53fc32549ffff4
Taleh Ibrahimli <talehsmail@gmail.com>
2023-11-29 21:05:55 +0400

    +fixes

--- sdk/spring/src/main/java/io/apibrew/client/spring/ConfigValues.java
+++ sdk/spring/src/main/java/io/apibrew/client/spring/ConfigValues.java
@@ -1,14 +1,20 @@
 package io.apibrew.client.spring;
 
 import io.apibrew.client.Config;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "apibrew")
 public class ConfigValues {
+
+    @Value("${apibrew.channelKey:default}")
     public String channelKey = "default";
+
+    @Value("${apibrew.serviceName:default}")
     String serviceName = "default";
+
     Config.Server server;
+
+    @Value("${apibrew.server.profile:default}")
     String profile;
 }
