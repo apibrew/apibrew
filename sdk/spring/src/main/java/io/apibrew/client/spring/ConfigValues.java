c9cb532c951d0aa9d07871d8bafaf2a866e161cd
Taleh Ibrahimli <talehsmail@gmail.com>
2023-11-29 20:48:12 +0400

    +fixes

--- sdk/spring/src/main/java/io/apibrew/client/spring/ConfigValues.java
+++ sdk/spring/src/main/java/io/apibrew/client/spring/ConfigValues.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "mail")
+@ConfigurationProperties(prefix = "apibrew")
 public class ConfigValues {
     public String channelKey = "default";
     String serviceName = "default";
