72411f067cd42bf32f372b10d555d4f35ca8aada
Taleh Ibrahimli <talehsmail@gmail.com>
2024-03-24 16:09:53 +0400

    minor fixes

--- pkg/test/resource_update_test.go
+++ pkg/test/resource_update_test.go
@@ -2,127 +2,125 @@ package test
 
 import (
 	"github.com/apibrew/apibrew/pkg/model"
-	"github.com/apibrew/apibrew/pkg/service/annotations"
 	"github.com/apibrew/apibrew/pkg/stub"
 	"github.com/apibrew/apibrew/pkg/test/setup"
 	"github.com/apibrew/apibrew/pkg/util"
-	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/types/known/structpb"
 	"testing"
 )
 
-func _ignord_TestPrepareResourceMigrationPlan(t *testing.T) {
-	resource1 := &model.Resource{
-		Name:      "test-resource-for-update-1",
-		Namespace: "default",
-		SourceConfig: &model.ResourceSourceConfig{
-			DataSource: setup.DhTest.Name,
-			Entity:     "test-resource-for-update-1",
-		},
-		Properties: []*model.ResourceProperty{
-			{
-				Name:     "prop-1",
-				Type:     model.ResourceProperty_STRING,
-				Length:   128,
-				Required: true,
-			}, {
-				Name:     "prop-2",
-				Type:     model.ResourceProperty_STRING,
-				Length:   128,
-				Required: true,
-				Annotations: map[string]string{
-					annotations.SourceMatchKey: "prop-2",
-				},
-			}, {
-				Name:     "prop-3",
-				Type:     model.ResourceProperty_STRING,
-				Length:   128,
-				Required: true,
-			},
-		},
-	}
-
-	resourceCreateRes, err := resourceClient.Create(setup.Ctx, &stub.CreateResourceRequest{Resources: []*model.Resource{resource1}, DoMigration: true})
-
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	defer func() {
-		if resourceCreateRes != nil {
-			_, _ = resourceClient.Delete(setup.Ctx, &stub.DeleteResourceRequest{
-				Ids:            []string{resourceCreateRes.Resources[0].Id},
-				DoMigration:    true,
-				ForceMigration: true,
-			})
-		}
-	}()
-
-	resource1 = &model.Resource{
-		Name:      "test-resource-for-update-1",
-		Namespace: "default",
-		SourceConfig: &model.ResourceSourceConfig{
-			DataSource: setup.DhTest.Name,
-			Entity:     "test-resource-for-update-1",
-		},
-		Properties: []*model.ResourceProperty{
-			{
-				Name:     "prop-1",
-				Type:     model.ResourceProperty_STRING,
-				Length:   128,
-				Required: true,
-			},
-			{
-				Name:     "prop-2a",
-				Type:     model.ResourceProperty_FLOAT32,
-				Length:   128,
-				Required: false,
-				Annotations: map[string]string{
-					annotations.SourceMatchKey: "prop-2",
-				},
-			},
-			{
-				Name:     "prop-5",
-				Type:     model.ResourceProperty_STRING,
-				Length:   127,
-				Required: false,
-			},
-		},
-	}
-
-	util.NormalizeResource(resource1)
-
-	resource1.Id = resourceCreateRes.Resources[0].Id
-
-	res, err := resourceClient.PrepareResourceMigrationPlan(setup.Ctx, &stub.PrepareResourceMigrationPlanRequest{
-		Resources: []*model.Resource{resource1},
-	})
-
-	if err != nil {
-		assert.Error(t, err)
-	}
-
-	assert.Len(t, res.Plans, 1)
-	assert.Len(t, res.Plans[0].Steps, 4)
-
-	if t.Failed() {
-		return
-	}
-
-	steps := res.Plans[0].Steps
-
-	assert.IsType(t, steps[0].Kind, &model.ResourceMigrationStep_UpdateResource{})
-	assert.IsType(t, steps[1].Kind, &model.ResourceMigrationStep_UpdateProperty{})
-	assert.IsType(t, steps[2].Kind, &model.ResourceMigrationStep_DeleteProperty{})
-	assert.IsType(t, steps[3].Kind, &model.ResourceMigrationStep_CreateProperty{})
-
-	if t.Failed() {
-		return
-	}
-
-	assert.Equal(t, steps[1].Kind.(*model.ResourceMigrationStep_UpdateProperty).UpdateProperty.ChangedFields, []string{"name", "type", "required"})
-}
+//func _ignord_TestPrepareResourceMigrationPlan(t *testing.T) {
+//	resource1 := &model.Resource{
+//		Name:      "test-resource-for-update-1",
+//		Namespace: "default",
+//		SourceConfig: &model.ResourceSourceConfig{
+//			DataSource: setup.DhTest.Name,
+//			Entity:     "test-resource-for-update-1",
+//		},
+//		Properties: []*model.ResourceProperty{
+//			{
+//				Name:     "prop-1",
+//				Type:     model.ResourceProperty_STRING,
+//				Length:   128,
+//				Required: true,
+//			}, {
+//				Name:     "prop-2",
+//				Type:     model.ResourceProperty_STRING,
+//				Length:   128,
+//				Required: true,
+//				Annotations: map[string]string{
+//					annotations.SourceMatchKey: "prop-2",
+//				},
+//			}, {
+//				Name:     "prop-3",
+//				Type:     model.ResourceProperty_STRING,
+//				Length:   128,
+//				Required: true,
+//			},
+//		},
+//	}
+//
+//	resourceCreateRes, err := resourceClient.Create(setup.Ctx, &stub.CreateResourceRequest{Resources: []*model.Resource{resource1}, DoMigration: true})
+//
+//	if err != nil {
+//		t.Error(err)
+//		return
+//	}
+//
+//	defer func() {
+//		if resourceCreateRes != nil {
+//			_, _ = resourceClient.Delete(setup.Ctx, &stub.DeleteResourceRequest{
+//				Ids:            []string{resourceCreateRes.Resources[0].Id},
+//				DoMigration:    true,
+//				ForceMigration: true,
+//			})
+//		}
+//	}()
+//
+//	resource1 = &model.Resource{
+//		Name:      "test-resource-for-update-1",
+//		Namespace: "default",
+//		SourceConfig: &model.ResourceSourceConfig{
+//			DataSource: setup.DhTest.Name,
+//			Entity:     "test-resource-for-update-1",
+//		},
+//		Properties: []*model.ResourceProperty{
+//			{
+//				Name:     "prop-1",
+//				Type:     model.ResourceProperty_STRING,
+//				Length:   128,
+//				Required: true,
+//			},
+//			{
+//				Name:     "prop-2a",
+//				Type:     model.ResourceProperty_FLOAT32,
+//				Length:   128,
+//				Required: false,
+//				Annotations: map[string]string{
+//					annotations.SourceMatchKey: "prop-2",
+//				},
+//			},
+//			{
+//				Name:     "prop-5",
+//				Type:     model.ResourceProperty_STRING,
+//				Length:   127,
+//				Required: false,
+//			},
+//		},
+//	}
+//
+//	util.NormalizeResource(resource1)
+//
+//	resource1.Id = resourceCreateRes.Resources[0].Id
+//
+//	res, err := resourceClient.PrepareResourceMigrationPlan(setup.Ctx, &stub.PrepareResourceMigrationPlanRequest{
+//		Resources: []*model.Resource{resource1},
+//	})
+//
+//	if err != nil {
+//		assert.Error(t, err)
+//	}
+//
+//	assert.Len(t, res.Plans, 1)
+//	assert.Len(t, res.Plans[0].Steps, 4)
+//
+//	if t.Failed() {
+//		return
+//	}
+//
+//	steps := res.Plans[0].Steps
+//
+//	assert.IsType(t, steps[0].Kind, &model.ResourceMigrationStep_UpdateResource{})
+//	assert.IsType(t, steps[1].Kind, &model.ResourceMigrationStep_UpdateProperty{})
+//	assert.IsType(t, steps[2].Kind, &model.ResourceMigrationStep_DeleteProperty{})
+//	assert.IsType(t, steps[3].Kind, &model.ResourceMigrationStep_CreateProperty{})
+//
+//	if t.Failed() {
+//		return
+//	}
+//
+//	assert.Equal(t, steps[1].Kind.(*model.ResourceMigrationStep_UpdateProperty).UpdateProperty.ChangedFields, []string{"name", "type", "required"})
+//}
 
 func TestResourceUpdateCreateNewPropertyAndMarkAsRequired(t *testing.T) {
 	resource1 := &model.Resource{
